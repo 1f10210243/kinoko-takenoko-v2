4b102afd2f9118d1d237a920d1fe9c799b8f1c82
Rin Nakamura <s1f102102436@iniad.org>
2021-06-29 10:02:07 +0900

    4

--- app.py
+++ app.py
@@ -12,7 +12,7 @@ def top():
 
 @app.route('/vote', methods=['POST'])
 def answer():
-    global kinoko_count, takenoko_count
+    global kinoko_count, takenoko_count,messages
     if request.form.get("item") == 'kinoko':
         kinoko_count += 1
     elif request.form.get("item") == 'takenoko':
@@ -24,6 +24,13 @@ def answer():
         
     kinoko_percent = kinoko_count / (kinoko_count + takenoko_count) * 100
     takenoko_percent = takenoko_count / (kinoko_count + takenoko_count) * 100
+    
+    message_html=''
+    for i in range(len(messages)):
+        message=messages[i]
+        message_html+='<div class="alert{1}"role="alert">{0}</div>\n'.format(
+            message,'alert-warning ms-5'if i%2==0 else'alert-success me-5')
+            
     return render_template('vote.html', **vars())
 
 if __name__ == '__main__':
